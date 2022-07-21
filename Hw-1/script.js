00b092d9fd1ca78f9160cbc14b38b1ac1ff45b72
NastasiakSS <nastasiak.sss@gmail.com>
2022-07-21 21:51:17 +0300

    Initial commit

---
+++ Hw-1/script.js
@@ -0,0 +1,70 @@
+/*
+- Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+    Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+let hello = 'hello';
+let space = ' ';
+let owu = 'owu';
+let com = 'com' ;
+let ua = 'ua' ;
+let n1 = 1;
+let n10= 10;
+let n999= -999;
+let n123= 123 ;
+let nPi= 3.14 ;
+let nNePi= 2.7 ;
+let n16= 16 ;
+let tr = true;
+let fl = false;
+
+
+
+
+alert(hello + space + owu + space + com + space + ua)
+document.write( n1 + space + n10 + space + n999 + space + n123 + space + nPi + space + nNePi + space + n16)
+console.log(tr)
+console.log(fl)*/
+
+
+/*===========================================================================================*/
+
+
+/*- Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)*/
+
+/*
+let firstName = 'Svyatoslav';
+let middleName = 'Ruslanovych';
+let lastName = 'Nastosiak';
+let space = ' ';
+
+let person = (firstName + space + middleName + space + lastName)
+
+console.log(person)*/
+
+
+/*===========================================================================================*/
+
+/*- За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+  let a = 100; let b = '100'; let c = true;*/
+
+/*
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)*/
+
+
+/*===========================================================================================*/
+
+
+/*- За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+*/
+/*let name = prompt('Name?' , 'Svytoslaw')
+let middleName = prompt('Middle Name?' , 'Ruslanovych')
+let age = prompt('Age?' , '17')
+console.log(name)
+console.log(middleName)
+console.log(age)*/
