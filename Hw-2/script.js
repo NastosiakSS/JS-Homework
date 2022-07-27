cfdb1fe6f9abe8273be684f3e6773c0de4efd2aa
NastasiakSS <nastasiak.sss@gmail.com>
2022-07-27 16:45:42 +0300

    hw-2

---
+++ Hw-2/script.js
@@ -0,0 +1,153 @@
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+
+/*let object = [
+    1.2,3,142,'hi','owu','name',true,false,'Svyatoslav',134
+]
+
+console.log( object)
+console.log(object[0])
+console.log(object[1])
+console.log(object[2])
+console.log(object[3])
+console.log(object[4])
+console.log(object[5])
+console.log(object[6])
+console.log(object[7])
+console.log(object[8])
+console.log(object[9])*/
+
+//- Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+
+/*let firstBook = {
+    title: 'Harry Potter and the Philosopher\'s Stone',
+    pageCount:352,
+    genre: 'Fantastic movie',
+}
+
+console.log(firstBook)
+
+let secondBook = {
+    title:'Harry Potter and the Prisoner of Azkaban book',
+    pageCount:317,
+    genre:'Fantastic movie',
+}
+
+console.log(secondBook)
+
+let thirdBook = {
+    title:'Harry Potter and the Chamber of Secrets',
+    pageCount:251,
+    genre:'Fantastic movie',
+}
+
+console.log(thirdBook)*/
+
+//- Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
+
+/*let firstBook = {
+    title: 'Harry Potter and the Philosopher\'s Stone',
+    pageCount: 352,
+    genre: 'Fantastic movie',
+    author: [{
+        name: 'Joanne Rowling',
+        age: 56
+    }
+    ]
+}
+
+console.log(firstBook)
+
+let secondBook = {
+    title: 'Harry Potter and the Prisoner of Azkaban',
+    pageCount: 317,
+    genre: 'Fantastic movie',
+    author: [{
+        name: 'Joanne Rowling',
+        age: 56
+    }
+    ]
+}
+
+console.log(secondBook)
+
+let thirdBook = {
+    title: 'Harry Potter and the Chamber of Secrets',
+    pageCount: 251,
+    genre: 'Fantastic movie',
+    author: [{
+        name: 'Joanne Rowling',
+        age: 56
+    }
+    ]
+}
+
+console.log(thirdBook)*/
+
+//- Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
+
+/*
+let users = [
+    {
+        name: 'Svyatoslav',
+        username: 'NastosiakSS',
+        password: 12112112,
+    },
+    {
+        name: 'Ruslan',
+        username: 'NastosiakRR',
+        password: 13113113,
+    },
+    {
+        name: 'Liudmula',
+        username: 'NastosiakLL',
+        password: 14114114,
+    },
+    {
+        name: 'Stepan',
+        username: 'StepaNRV',
+        password: 15115115,
+    },
+    {
+        name: 'Ivan',
+        username: 'IweN',
+        password: 16116116,
+    },
+    {
+        name: 'Andriy',
+        username: 'AnDrew',
+        password: 17117117,
+    },
+    {
+        name: 'Vitaliy',
+        username: 'Vitaliy321',
+        password: 18118118,
+    },
+    {
+        name: 'Volodymyr',
+        username: 'FVvolodymyr',
+        password: 19119119,
+    },
+    {
+        name: 'Orest',
+        username: 'SkOrest',
+        password: 19811981198,
+    },
+    {
+        name: 'Borys',
+        username: 'BorySFR',
+        password: 10110110,
+    }
+]
+
+console.log(users)
+
+console.log(users[0].password)
+console.log(users[1].password)
+console.log(users[2].password)
+console.log(users[3].password)
+console.log(users[4].password)
+console.log(users[5].password)
+console.log(users[6].password)
+console.log(users[7].password)
+console.log(users[8].password)
+console.log(users[9].password)*/
